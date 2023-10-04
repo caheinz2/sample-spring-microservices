fc7fb3dfb842f082b5ba6faa4972f4f300ae44eb
Caleb Heinz <caleb.heinz@7factor.io>
2023-10-04 18:42:32 -0400

    Commenting out code to start demo

--- department-service/src/main/java/pl/piomin/services/department/repository/DepartmentRepository.java
+++ department-service/src/main/java/pl/piomin/services/department/repository/DepartmentRepository.java
@@ -1,5 +1,6 @@
 package pl.piomin.services.department.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import pl.piomin.services.department.entity.Department;
 
